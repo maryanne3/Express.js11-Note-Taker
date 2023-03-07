3f2656a145030b22cf6e28a4349d7dc28eae6b1e
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-03-07 20:22:24 +0000

    intial commit

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// create express server
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+const routes = require("./source/route");
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-// get routes
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 
-// Express middleware
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(routes);
 
-// Use apiRoutes
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
-
-// app is listening to the port
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
