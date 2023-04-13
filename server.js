4d37d0643c3cc695bf1790ed29b1e2fe4071cc6b
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-04-13 17:33:49 +0100

    edits

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
-const routes = require("./source/route");
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+// Get Routes
+const apiRoutes = require("./source/routes/apiRoutes");
+const htmlRoutes = require("./source/routes/htmlRoutes");
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
